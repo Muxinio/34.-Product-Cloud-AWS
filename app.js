519318e07f014efc005f8b03ccb90c1d634f5c30
Lucas Gaston Setaro <75910634+Muxinio@users.noreply.github.com>
2023-02-04 01:44:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 
-
-
 app.get('/users', (req, res) => {
     res.send({ mensaje: `Aqui los users` });
 })
 
 app.set("port", process.env.PORT || 8080);
 app.listen(app.get("port"), () => {
-    console.log("Server Running in Port " + app.get("port"))
+    console.log("TestServer Online " + app.get("port"))
 });
